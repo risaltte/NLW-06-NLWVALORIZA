1b5eee56f6ad80d6ed59b689b2197bb039568757
Rafael Risalte <risaltte@gmail.com>
2021-06-21 22:35:42 -0400

    Aula 02 - Maximum Speed

---
+++ src/entities/User.ts
@@ -0,0 +1,34 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+class User {
+
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    admin: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    constructor() {
+        // Se não tiver id, criar um id
+        if (!this.id) {
+           this.id = uuid(); 
+        }
+    }
+
+}
+
+export { User };
